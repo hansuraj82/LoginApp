e2996f215f4fe48eae837be09a1a10667c9a7fcc
Hansu <hansur812@gmail.com>
2024-09-06 13:44:20 +0530

    change the origin of cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 //Using middlewares
 app.use(express.json());
 const corsOptions = {
-    origin: 'https://login-app-liart.vercel.app/', // Allow requests from this origin
+    origin: 'https://login-app-liart.vercel.app', // Allow requests from this origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only these methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Adjust the allowed headers if necessary
   };
