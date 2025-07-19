1f40e963eb30291a538d14cb65366e75982ba938
Hansu <hansur812@gmail.com>
2025-07-19 17:45:15 +0530

    the rest data is except password,_id,createdAt,updatedAt for security purpose.

--- server/Routes/getUser.js
+++ server/Routes/getUser.js
@@ -10,7 +10,7 @@ router.get('/user/:username', async (req,res)=> {
         const user = await userModel.findOne({username});
         if(!user) return res.status(404).json({error:"can't find the user in the database"});
         const {password,_id,createdAt,updatedAt,...rest} = Object.assign({},user.toJSON())
-        return res.status(200).json(rest);
+        return res.status(200).json(rest);//rest is returned because i don;t want to rend the confidential data with the rest data of a user
     } catch (error) {
         console.log(error)
         return res.status(501).json({error: "can't find the user"})
